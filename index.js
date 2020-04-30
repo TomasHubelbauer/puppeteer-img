783ef5596b6b1a28403b1679e9166908b95ebccd
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-04-30 20:48:52 +0200

    Demonstrate video screenshot taking

--- index.js
+++ index.js
@@ -15,6 +15,35 @@ void async function () {
   });
 
   const clip = await page.evaluate(() => document.images[0].getBoundingClientRect().toJSON());
-  await page.screenshot({ path: path.join(__dirname, 'screenshot.png'), clip });
+  await page.screenshot({ path: path.join(__dirname, 'img-screenshot.png'), clip });
+  await browser.close();
+}()
+
+void async function () {
+  const browser = await puppeteer.launch({ headless: false });
+  const [page] = await browser.pages();
+  await page.goto('file://' + path.join(__dirname, 'HERO_-_Blender_Open_Movie-full_movie.webm.360p.vp9.webm'));
+  await page.waitForFunction(() => document.getElementsByTagName('video')[0].readyState === 4);
+
+  await page.evaluate(() => {
+    /** @type {HTMLVideoElement} */
+    const videoVideo = document.getElementsByTagName('video')[0];
+
+    // Hide video controls so that they don't appear in the screenshot
+    videoVideo.controls = false;
+
+    // Scale down to a poster size
+    videoVideo.style.maxWidth = 200;
+    videoVideo.style.maxHeight = 200;
+
+    // Seek to 10 % to skip likely blank frames at the start
+    videoVideo.currentTime = videoVideo.duration * .1;
+  });
+
+  // Wait for the video to stop seeking before capturing the screenshot
+  await page.waitForFunction(() => !document.getElementsByTagName('video')[0].seeking);
+
+  const clip = await page.evaluate(() => document.getElementsByTagName('video')[0].getBoundingClientRect().toJSON());
+  await page.screenshot({ path: path.join(__dirname, 'video-screenshot.png'), clip });
   await browser.close();
 }()
